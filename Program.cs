c7e560afc28e8d146bf4b664dcd43a91e6b47d83
SharkyIX <89183770+SharkyIX@users.noreply.github.com>
2021-08-20 19:59:18 +0200

    very minor changes

--- Program.cs
+++ Program.cs
@@ -6,26 +6,26 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please type a number to check if it is an even or odd number.");
+            Console.WriteLine("Type a number to check if it is odd or even.");
             string userInput;
             userInput = Console.ReadLine();
             //checks if number is larger than zero
             while ( Int32.Parse(userInput) <= 0)
             {
-                Console.WriteLine("Please enter a number greater than 0!");
+                Console.WriteLine("Enter a number greater than 0!");
                 userInput = Console.ReadLine();
             }
             //checks if number is odd or even
             if (Int32.Parse(userInput) % 2 == 0)
             {
-                Console.WriteLine("Number is even");
+                Console.WriteLine("The number is even");
             }
             else
             {
-                Console.WriteLine("Number is odd");
+                Console.WriteLine("The number is odd");
             }
             //exit for application
-            Console.WriteLine("Please press enter to exit...");
+            Console.WriteLine("Please press enter to exit!");
             Console.ReadLine();
         }
     }
